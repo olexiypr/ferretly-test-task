08506388ad37565e8c65dc6ad228a15cfda547e9
oleksii <oleksii@office.cquelle.de>
2025-03-23 10:18:52 +0200

    Add comment to tests

--- Ferretly.TestTask.TimeTrackingApi.Tests/UnitTest1.cs
+++ Ferretly.TestTask.TimeTrackingApi.Tests/UnitTest1.cs
@@ -1,9 +1,11 @@
 ﻿namespace Ferretly.TestTask.TimeTrackingApi.Tests;
 
+//As we don't have much business-logic, in my opinion integration tests are more suitable 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        
     }
 }
